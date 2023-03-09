f6176d76f9ca5fc995263e65aff03b6e7c82cb69
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-03-09 23:00:54 +0100

    Add HTML and styling with Tailwind
    Co-authored-by: Robin kron <Rkron91@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cblue: "#112D4E",
+        clightblue: "#8FC3FF"
+      }
+    },
   },
   plugins: [],
 }
