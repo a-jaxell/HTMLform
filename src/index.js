49f96de9b9996af3bb027246ad6b507d971e7e24
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-03-09 23:01:09 +0100

    Add JS and commentary

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+/* TODO
+    
+    Visa hur starkt lösenordet är 1-4
+    <meter>
+    https://css-tricks.com/password-strength-meter/
+
+    Skicka informationen med submit till objekt i JS.
+
+
+*/
